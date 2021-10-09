7022450bf714202baf63349056725d66b9ddd1b2
Rúben Israel <rubenisrael974@gmail.com>
2021-10-09 19:34:11 -0300

    Create Banir_palavras_inadequadas.py

---
+++ Banir_palavras_inadequadas.py
@@ -0,0 +1,10 @@
+
+similar = ['a', '4', '@'], ['e', '3', '&'], ['i', '1', '!'], ['o', '0'] # Caracteres similares
+inadequado = ['4', '@', '3', '&', '1', '!', '0'] # Caracteres inadequados
+
+
+txt = 'Abbe3&&lhaaa@4@4a' # Texto para ser analizado
+txt = txt.lower() # Deixando o txt em minusculo 
+
+
+
