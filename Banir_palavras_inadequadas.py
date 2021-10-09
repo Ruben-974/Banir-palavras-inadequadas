1c8197a8c8740dff6b1c58fd8d8e05afb9943f6b
Rúben Israel <rubenisrael974@gmail.com>
2021-10-09 19:35:45 -0300

    Substituindo caracteres similares

--- Banir_palavras_inadequadas.py
+++ Banir_palavras_inadequadas.py
@@ -6,5 +6,15 @@ inadequado = ['4', '@', '3', '&', '1', '!', '0'] # Caracteres inadequados
 txt = 'Abbe3&&lhaaa@4@4a' # Texto para ser analizado
 txt = txt.lower() # Deixando o txt em minusculo 
 
+lista_caracters = []
 
+for c in txt: 
+    lista_caracters.append(c) # Adicioando todos caracteres de "txt" na lista "lista_caracters"
+
+for c in range(len(similar)): 
+    for c1 in range(len(lista_caracters)): 
+        if lista_caracters[c1] in similar[c]: # Se o caracter da "lista_caracters" estiver no item "similar"
+            lista_caracters[c1] = similar[c][0] # O caracter similar recebe o caracter original
+
+txt = ''.join(lista_caracters) # "txt" recebe o conteúdo da "lista_caracters" junto
 
