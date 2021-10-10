e765386dbc8559e237992c6fbdf83022006d34bd
Rúben Israel <rubenisrael974@gmail.com>
2021-10-09 21:53:13 -0300

    Analisando a frase e censurando palavras indesejadas

--- Banir_palavras_inadequadas.py
+++ Banir_palavras_inadequadas.py
@@ -1,9 +1,9 @@
 
-similar = ['a', '4', '@'], ['e', '3', '&'], ['i', '1', '!'], ['o', '0'] # Caracteres similares
+similar = ['a', '4', '@'], ['e', '3', '&'], ['i', '1', '!'], ['o', '0'], [' ', '-', '_', '/', '|'] # Caracteres similares
 inadequado = ['4', '@', '3', '&', '1', '!', '0'] # Caracteres inadequados
-indesejadas = ['abelha'] # palavras indesejadas
+indesejadas = ['abelhas', 'gosto'] # palavras indesejadas
 
-txt = 'Eeee333uuu ggoostt00oo dde @bbe3&&lllhhasssss'.lower().strip()
+txt = '----Eeee333uuu-ggoostt00oo___dde| @bbe3&&lllhhasssss///'.lower().strip()
 
 print(txt)
 
@@ -40,9 +40,34 @@ for c in range(len(txt)):
 if len(dell) > 0 and dell[0] == dell[-1]:
     txt_temp = txt_temp.replace(dell, '', 1) # Repetidos = Principal | Ex: 'eeeeee' = 'e'
 
-txt = txt_temp
+txt = txt_temp.strip()
+
+lista_palavras, palavra = [], []
+
+# Criando uma lista só com as palavras
+
+for c in txt:
+
+    if c not in ' ':
+        palavra.append(c)
+    else:
+        lista_palavras.append(''.join(palavra[:]))
+        palavra = []
+
+lista_palavras.append(''.join(palavra[:]))
+
+# Modificando a palavra se ele for indesejada
 
 print(txt)
 
-#if txt in indesejadas:
-#    print(f'{txt[0:2]}{(len(txt)-2)*"*"}')
+for c in range(len(lista_palavras)):
+    if lista_palavras[c] in indesejadas:
+        lista_palavras[c] = f'{lista_palavras[c][0]}{(len(lista_palavras[c])-1)*"*"}'
+
+txt = ' '.join(lista_palavras)
+
+print(txt)
+
+# Frase inicial     = ----eeee333uuu-ggoostt00oo___dde| @bbe3&&lllhhasssss///
+# Frase sem censura = eu gosto de abelhas
+# Frase com censura = eu g**** de a******
