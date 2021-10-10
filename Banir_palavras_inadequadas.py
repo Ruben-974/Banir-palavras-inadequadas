144254ea3136a1138430c60680aa9f9229355ace
Rúben Israel <rubenisrael974@gmail.com>
2021-10-09 21:06:33 -0300

    Substitui os caracteres repetidos em apenas um

--- Banir_palavras_inadequadas.py
+++ Banir_palavras_inadequadas.py
@@ -2,19 +2,44 @@
 similar = ['a', '4', '@'], ['e', '3', '&'], ['i', '1', '!'], ['o', '0'] # Caracteres similares
 inadequado = ['4', '@', '3', '&', '1', '!', '0'] # Caracteres inadequados
 
-
-txt = 'Abbe3&&lhaaa@4@4a' # Texto para ser analizado
-txt = txt.lower() # Deixando o txt em minusculo 
+txt = '@bbe3&&lllhhaaa@4@4a'.lower()
 
 lista_caracters = []
 
+# Irá Substituir os caracteres similares | Ex: '3' = 'e'
+
 for c in txt: 
-    lista_caracters.append(c) # Adicioando todos caracteres de "txt" na lista "lista_caracters"
+    lista_caracters.append(c)
 
 for c in range(len(similar)): 
     for c1 in range(len(lista_caracters)): 
-        if lista_caracters[c1] in similar[c]: # Se o caracter da "lista_caracters" estiver no item "similar"
-            lista_caracters[c1] = similar[c][0] # O caracter similar recebe o caracter original
+        if lista_caracters[c1] in similar[c]:
+            lista_caracters[c1] = similar[c][0] # Similar = Original | Ex: @ = a
 
-txt = ''.join(lista_caracters) # "txt" recebe o conteúdo da "lista_caracters" junto
+txt = ''.join(lista_caracters)
 
+txt_temp, dell = txt, ''
+
+# Irá substituir os caracteres repetidos em apenas um | Ex: 'aaaaaaa' = 'a'
+
+for c in range(len(txt)):
+
+    if c+1 < len(txt):
+
+        if txt[c] == txt[c+1]:
+            
+            dell += txt[c]
+
+        elif txt[c] != txt[c+1] and len(dell) != 0:
+
+            txt_temp= txt_temp.replace(dell+dell[0], dell[0], 1) # Repetidos = Principal | Ex: 'eeeeee' = 'e'
+            dell = ''
+
+# Susbstitui os ultimos caracteres se necessario
+
+if txt_temp[0] == txt_temp[-1] and len(dell) != 0:
+    txt_temp = txt_temp.replace(dell, '', 1) # Repetidos = Principal | Ex: 'eeeeee' = 'e'
+
+txt = txt_temp
+
+print(txt)
